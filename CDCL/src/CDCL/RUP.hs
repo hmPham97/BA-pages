30a6fe277d4dbc3aed11bb11375b0c075f1f8a3e
hm-pham7 <pham7@hm.edu>
2021-06-23 18:41:16 +0200

    RUP started. (need consultation)

---
+++ CDCL/src/CDCL/RUP.hs
@@ -0,0 +1,22 @@
+module CDCL.RUP where 
+
+import CDCL.Types
+import CDCL.MapLogic
+
+startRUP :: ClauseList -> TupleClauseList -> MappedTupleList -> [Clause]
+startRUP = rup
+
+rup :: ClauseList -> TupleClauseList -> MappedTupleList  -> [Clause]
+rup _ _ _ = []
+
+setToFalse :: ClauseList -> MappedTupleList -> (ClauseList, MappedTupleList) 
+setToFalse (xs : ys) mtl = (ys , setToFalse' (getClauseFromReducedClauseAndOGClause xs) mtl)
+
+
+setToFalse' :: Clause -> MappedTupleList -> MappedTupleList 
+setToFalse' (xs : ys) mtl = setToFalse' ys updated
+    where curX = getVariableValue xs
+          updated = if curX < 0 then pushToMappedTupleList mtl (Level 0) (xs, BTrue) Decision else pushToMappedTupleList mtl (Level 0) (xs, BFalse) Decision
+
+setToFalse' [] mtl = mtl
+
