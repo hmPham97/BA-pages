9a27c3cf3f20124d8d2c2cd87ff41ffd30f0a2ab
hm-pham7 <pham7@hm.edu>
2021-08-07 17:26:23 +0200

    deleted more

--- CDCL/profiteur.sh
+++
@@ -1,3 +0,0 @@
-stack build --profile
-stack run --profile -- $ARGUMENTE +RTS -p -RTS
-profiteur CDCL-exe.EXE.prof
--- CDCL/stylish.sh
+++
@@ -1,9 +0,0 @@
-stylish-haskell -i .\\src\\CDCL\\Algorithm.hs
-stylish-haskell -i .\\src\\CDCL\\Types.hs
-stylish-haskell -i .\\src\\CDCL\\UnitPropagation.hs
-stylish-haskell -i .\\src\\CDCL\\Decisionalalgorithm.hs
-stylish-haskell -i .\\src\\CDCL\\MapLogic.hs
-stylish-haskell -i .\\src\\CDCL\\Conflict.hs
-stylish-haskell -i .\\src\\CDCL\\CDCLFilereader.hs
-stylish-haskell -i .\\test\\AlgorithmSpec.hs
-stylish-haskell -i .\\app\\Main.hs
